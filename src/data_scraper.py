4c3cf022a6fddec50eb79efddf39cca5d2dfec8f
jpang32 <jpangallozzi@gmail.com>
2023-12-24 17:52:47 -0500

    Chore: splitting up functions

--- src/data_scraper.py
+++ src/data_scraper.py
@@ -6,34 +6,56 @@ from bs4 import BeautifulSoup
 from urllib.parse import quote
 from xml.etree import ElementTree as ET
 
-HARVARD_SERVER_URL = os.getenv("HARVARD_SERVER_URL")
+HARVARD_DATAVERSE_URL = os.getenv("HARVARD_DATAVERSE_URL")
 HARVARD_DATAVERSE_TOKEN = os.getenv("HARVARD_DATAVERSE_TOKEN")
 
 
-def main(title_search_terms: list):
-    for title_search_term in title_search_terms:
-        search_url = f"{HARVARD_SERVER_URL}/api/search?q=title:*{title_search_term}*&type=dataset"
-        search_response = requests.get(search_url, headers={"X-Dataverse-key": HARVARD_DATAVERSE_TOKEN}).json()
-        search_items = search_response["data"]["items"]
+def search_dataverse_for_datasets(title_search_term: list) -> list:
+    search_url = f"{HARVARD_DATAVERSE_URL}/api/search?q=title:*{title_search_term}*&type=dataset"
+    search_response = requests.get(search_url, headers={"X-Dataverse-key": HARVARD_DATAVERSE_TOKEN}).json()
+    return search_response["data"]["items"]
 
-        # For each dataset, call Dataverse API to get information on files within dataset
-        # Then, for each file, call Dataverse API to get metadata for file
-        # Then end result will be a desctiption of the dataset and a list containing the metadata for each file
-        for item in search_items:
-            dataset_url = f"{HARVARD_SERVER_URL}/api/datasets/:persistentId/?persistentId={item['global_id']}"
-            dataset_response = requests.get(dataset_url).json()
-            dataset_id = dataset_response["data"]["id"]
-            version_id = dataset_response["data"]["latestVersion"]["versionNumber"]
 
-            files_url = f"{HARVARD_SERVER_URL}/api/datasets/{dataset_id}/versions/{version_id}/files?key={HARVARD_DATAVERSE_TOKEN}"
-            files_response = requests.get(files_url).json()
-            file_id = files_response["data"][0]["dataFile"]["id"]
+def get_dataset_id_and_version_id(global_id: str) -> tuple:
+    dataset_url = f"{HARVARD_DATAVERSE_URL}/api/datasets/:persistentId/?persistentId={global_id}"
+    dataset_response = requests.get(dataset_url).json()
+    dataset_id = dataset_response["data"]["id"]
+    version_id = dataset_response["data"]["latestVersion"]["versionNumber"]
+    return dataset_id, version_id
 
-            file_metadata_url = f"{HARVARD_SERVER_URL}/api/access/datafile/{file_id}/metadata/ddi"
-            file_metadata_response = requests.get(file_metadata_url)
-            parsed_metadata_response = xmltodict.parse(file_metadata_response.text)
 
+def get_file_ids_from_dataset(dataset_id: str, version_id: str) -> list:
+    files_url = f"{HARVARD_DATAVERSE_URL}/api/datasets/{dataset_id}/versions/{version_id}/files?key={HARVARD_DATAVERSE_TOKEN}"
+    files_response = requests.get(files_url).json()
+    return [file["dataFile"]["id"] for file in files_response["data"]]
+
+
+def get_file_metadata(file_id: str) -> dict:
+    file_metadata_url = f"{HARVARD_DATAVERSE_URL}/api/access/datafile/{file_id}/metadata/ddi"
+    file_metadata_response = requests.get(file_metadata_url)
+    if not file_metadata_response.ok:
+        raise Exception(f"Failed to get metadata for file {file_id}: {file_metadata_response.text}")
+    parsed_metadata_response = xmltodict.parse(file_metadata_response.text)
     return parsed_metadata_response
+
+
+def main(title_search_terms: list):
+    result = ()
+
+    for title_search_term in title_search_terms:
+        search_items = search_dataverse_for_datasets(title_search_term)
+        for item in search_items:
+            dataset_id, version_id = get_dataset_id_and_version_id(item["global_id"])
+            file_ids = get_file_ids_from_dataset(dataset_id, version_id)
+            for file_id in file_ids:
+                try:
+                    file_metadata = get_file_metadata(file_id)
+                except:
+                    print(f"Failed to get metadata for file {file_id}, may not be tabular data.\n Skipping over this "
+                          f"dataset.")
+
+    return result
+
 
 if __name__ == '__main__':
     title_search_terms = [
